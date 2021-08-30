ff593d82ea5613d18a468f43fffebcbb13c5c409
James Lourd Virtudazo <virtjameslourd55@gmail.com>
2021-08-30 23:21:45 +0800

    Added Templates and Nav Bar

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/register.html"));
 });
 
 app.listen(port, () => {
