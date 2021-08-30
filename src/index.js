9d9998f8bd4cc593bb580378e008027eca039525
James Lourd Virtudazo <virtjameslourd55@gmail.com>
2021-08-30 21:10:04 +0800

    Renamed app.js to index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/profile", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/about.html"));
 });
 
 app.listen(port, () => {
